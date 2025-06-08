a87dad4a892a2ccf858890a186387b334e1018a8
Fluhhfy <nolan.bryson@icloud.com>
2025-06-07 17:25:19 -0700

    Course Work (June 7th 2025)

---
+++ PraticeDeployment/model.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+# Load the dataset
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+# Train the model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+
+# force no compression (so it’s just a raw pickle)
+joblib.dump(model, 'iris_model.pkl', compress=0)
